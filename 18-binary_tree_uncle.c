676154730c2201f2a5ad014ec01583138e36155b
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-04-11 14:00:54 -0400

    binary

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,14 +8,12 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL ||
+	    node->parent->parent == NULL)
 		return (NULL);
 
-	if (node == node->parent->left)
-		return (node->parent->parent->left);
-
-	if (node == node->parent->right)
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 
-	return (NULL);
+	return (node->parent->parent->left);
 }
